2da001e232113bcb7859aba9f6276177fadd8477
Nolan Picini <nolanpic@gmail.com>
2019-12-10 10:53:11 -0800

    Roughed in number components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 //import any components needed
+import NumberButton from './NumberButton';
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
 import { numbers } from '../../../data' 
@@ -15,6 +16,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map(num => <NumberButton number={num} />)}
     </div>
   );
 };
+
+export default Numbers;
